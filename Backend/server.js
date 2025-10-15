219bf4a9b3815ec50a05b8dbeb1339173463c32c
Pascal kambou <pascalkambou200@gmail.com>
2025-10-15 05:12:42 +0200

    Gestionnaire final

--- Backend/server.js
+++ Backend/server.js
@@ -11,7 +11,9 @@ const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(cors());
+app.use(cors({
+    origin: 'https://gestionnaire-d-taches.netlify.app'
+}));
 app.use(express.json());
 
 
